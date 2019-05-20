64c9ce2da006522efe614ef467bff3d1c1070f06
Krzysztof Szewczyk <krzysztof-szewczyk@o2.pl>
2019-05-20 03:51:53 +0200

    Final configuration change commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books_db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///books_db.sqlite3'
 app.config['SECRET_KEY'] = 'secret_stx_next_app'
 db = SQLAlchemy(app)
 
