a62ab0fde52e17dfcd7d965a865b87dab5c10eb7
Krzysztof Szewczyk <krzysztof-szewczyk@o2.pl>
2019-05-20 05:42:53 +0200

    Database changes 2 commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 import os
 
 
@@ -8,5 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = 'secret_stx_next_app'
 db = SQLAlchemy(app)
 
+from app.models import Book, Author, Category
+
+db.create_all()
+db.session.commit()
 
 from app import routes, models, errors
