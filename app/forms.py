20d59c423502b423cc0a6e28d0603aac58bb0973
Krzysztof Szewczyk <krzysztof-szewczyk@o2.pl>
2019-05-23 10:46:29 +0200

    Small visual change

--- app/forms.py
+++ app/forms.py
@@ -5,24 +5,24 @@ from wtforms.validators import DataRequired
 
 # used in add_book.html
 class AddBookForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()], render_kw={'placeholder': 'title'})
-    authors = StringField('Author', validators=[DataRequired()], render_kw={'placeholder': 'e.g. auth1, auth2, auth3'})
+    title = StringField('Title', validators=[DataRequired()], render_kw={'placeholder': 'Title'})
+    authors = StringField('Author', validators=[DataRequired()], render_kw={'placeholder': 'E.g. auth1, auth2, auth3'})
     categories = StringField('Category', validators=[DataRequired()],
-                             render_kw={'placeholder': 'e.g. cat1, cat2, cat3'})
-    description = TextAreaField('Description', validators=[DataRequired()], render_kw={'placeholder': 'description'})
+                             render_kw={'placeholder': 'E.g. cat1, cat2, cat3'})
+    description = TextAreaField('Description', validators=[DataRequired()], render_kw={'placeholder': 'Description'})
     submit = SubmitField('Add Book')
 
 
 # used in list.html
 class AuthorFilter(FlaskForm):
-    author_name = StringField('', render_kw={'placeholder': 'filter'})
+    author_name = StringField('', render_kw={'placeholder': 'Filter'})
 
 
 class CategoryFilter(FlaskForm):
-    category_name = StringField('', render_kw={'placeholder': 'filter'})
+    category_name = StringField('', render_kw={'placeholder': 'Filter'})
 
 
 # used in import_book.html
 class ImportBookForm(FlaskForm):
-    term = StringField('Search term', validators=[DataRequired()], render_kw={'placeholder': 'term'})
+    term = StringField('Search term', validators=[DataRequired()], render_kw={'placeholder': 'Term'})
     submit = SubmitField('Import')
